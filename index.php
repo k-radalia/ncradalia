36f9b5c86097941fd6e092c3b7707db973bb46c1
k-radalia <k-radalia@onu.edu>
2017-01-26 09:58:19 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <title>Hello World Test</title>
+        <link rel="stylesheet" type="text/css" href="main.css">
+    </head>
+    <body>
+        Hello World! Welcome to my <span>Audit for the Author</span>. 
+        <p>This is a site containing my various works and poems whether 
+            dealing with situations in my life currently or with real life deep philosophy.</p>
+    </
+    </body>
+</html>
